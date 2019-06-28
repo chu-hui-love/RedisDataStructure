654f2b15da51406b1bd3ff6b9535a13846cb77bd
chuhui <lovehui131125@gmail.com>
2019-06-28 16:59:07 +0800

    redis source code

--- src/adlist.c
+++ src/adlist.c
@@ -1,9 +1,55 @@
 /*
  * 从redis中抽取出来的双向链表
- *
- /
+ */
 
-#include"adlist.h"
+#include <stdlib.h>
+#include "adlist.h"
+
+
+list *listCreate(void){
+
+    struct list *list;
+
+    if ((list = malloc(sizeof(*list))) == NULL)
+        return NULL;
+	
+    list->head = list->tail = NULL;
+    list->len = 0;
+    list->dup = NULL;
+    list->free = NULL;
+    list->match = NULL;
+    return list;
+	 
+
+}
+
+void listRelease(list *list){
+
+
+
+}
+void listEmpty(list *list){
+
+ 
+	
+
+}
+list *listAddNodeHead(list *list, void *value);
+list *listAddNodeTail(list *list, void *value);
+list *listInsertNode(list *list, listNode *old_node, void *value, int after);
+void listDelNode(list *list, listNode *node);
+listIter *listGetIterator(list *list, int direction);
+listNode *listNext(listIter *iter);
+void listReleaseIterator(listIter *iter);
+list *listDup(list *orig);
+listNode *listSearchKey(list *list, void *key);
+listNode *listIndex(list *list, long index);
+void listRewind(list *list, listIter *li);
+void listRewindTail(list *list, listIter *li);
+void listRotate(list *list);
+void listJoin(list *l, list *o);
+
+
 
 
 
